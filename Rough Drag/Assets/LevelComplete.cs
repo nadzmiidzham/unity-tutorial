5f5692fa77f3475ba45fe02f13646bd91bb7bd40
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-05-24 10:44:50 +0800

    finished episode 9

---
+++ Rough Drag/Assets/LevelComplete.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+    public void LoadNextlevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
